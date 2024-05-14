be71ba86f7e2639c1c0980b1fd35cff65d47eb04
thesajalghosh <sajalghosh.itrd@gmail.com>
2024-05-14 13:22:48 +0530

    making changes in header

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -1,7 +1,22 @@
 import React from "react";
+import "./Header.css";
+import { IoReorderThree } from "react-icons/io5";
+import { IoIosSearch } from "react-icons/io";
 
 const Header = () => {
-  return <div>Header</div>;
+  return (
+    <div>
+      <div className="mobile__device__header">
+        <div className="mobile__device__header__container">
+          <div className="header__left__part">The Abani</div>
+          <div className="header__right__part">
+            <IoIosSearch size={30} />
+            <IoReorderThree size={30} />
+          </div>
+        </div>
+      </div>
+    </div>
+  );
 };
 
 export default Header;
