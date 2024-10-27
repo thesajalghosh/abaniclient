3e8cc2416b391c8951910f61269b238c032d364d
Sajal Ghosh <sajalghosh.itrd@gmail.com>
2024-10-27 13:47:03 +0530

    account page

--- src/pages/HomePage/HomePage.js
+++ src/pages/HomePage/HomePage.js
@@ -7,20 +7,49 @@ import CategoryComponent from "../../components/CategoryComponent/CategoryCompon
 const HomePage = () => {
   return (
     <div className="home_page_whole_container">
-    {/* <div className="current_location_section">
+      {/* <div className="current_location_section">
       <div>Dum Dum Road</div>
       <div>Kolkata</div>
     </div> */}
-    <div className = "home_page_search_box">
-      <div className="seach_box_container">
-        <IoSearch size={21}/>
-        <input placeholder="Search any hair service"/>
+      <div className="home_page_search_box">
+        <div className="seach_box_container">
+          <IoSearch size={21} />
+          <input placeholder="Search any hair service" />
+        </div>
       </div>
-    </div>
-    <div className="home_page_category_componant">
-      <CategoryComponent/>
-    </div>
+      <div className="home_page_category_componant">
+        <CategoryComponent />
+      </div>
       <BookCrousel />
+      <div className="home_page_footer_container">
+        <div className="company_name">The Abani</div>
+        <div className="footer_section">
+          <div className="footer_section_element_container">
+
+            <div className="footer_sub_heading">Company</div>
+            <div className="footer_section_element">
+              <span>About us</span>
+              <span>Terms @ condition</span>
+              <span>Privacy policy</span>
+              <span>Anti-discrimination policy</span>
+              <span>The Abani impact</span>
+            </div>
+          </div>
+          <div className="footer_section_element_container">
+
+            <div className="footer_sub_heading">For Customers</div>
+            <div className="footer_section_element">
+              <span>TA reviews</span>
+              <span>Categories near you</span>
+              <span>Blog</span>
+              <span>Contact</span>
+
+            </div>
+          </div>
+
+        </div>
+
+      </div>
     </div>
   );
 };
