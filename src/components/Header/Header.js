b0cd322b51aec48972a7ef30a6b2ed025d998f51
Sajal Ghosh <sajalghosh.itrd@gmail.com>
2025-01-26 20:50:36 +0530

    sdf

--- src/components/Header/Header.js
+++ src/components/Header/Header.js
@@ -5,6 +5,7 @@ import { IoIosHeartEmpty, IoIosSearch } from "react-icons/io";
 import {isEmpty}  from "../../utils/CommonUtilsFunction.js"
 import { IoIosArrowRoundBack } from "react-icons/io";
 import { useNavigate } from "react-router";
+import Logo_image from "../../Assets/abani_logo.jpg"
 
 const Header = ({backButton, backButtonFunction, headerEle}) => {
   const navigate = useNavigate()
@@ -22,7 +23,19 @@ const Header = ({backButton, backButtonFunction, headerEle}) => {
     <div>
       <div className="mobile__device__header">
         <div className="mobile__device__header__container">
-          <div className="header__left__part">{backButton && <IoIosArrowRoundBack size={30} onClick={()=> backFunction()}/>}{isEmpty(headerEle) ? "The Abani": headerEle}</div>
+          <div className="header__left__part">
+          {backButton &&
+           <IoIosArrowRoundBack size={30} onClick={()=> backFunction()}/>}
+           {isEmpty(headerEle) ?
+           <div className="header__logo__container">
+            <img src={Logo_image} alt="logo" className="header__logo" />
+<span>
+
+            The Abani
+</span>
+           </div>
+            :
+             headerEle}</div>
           <div className="header__right__part">
             <IoIosSearch size={30} />
             <IoReorderThree size={30} />
